5efaa206138171d81247f684d6d48d734dd3f308
Fabio Schunig <fabio.schunig@gmail.com>
2017-11-28 12:58:41 -0200

    v1.0 -
    - Abertura de arquivos pelo Windows
    - Configurações em arquivo INI
    - Abertura e lista de arquivos
    - Edição do filtro do DataSet
    - Escolha de colunas visíveis
    - Ordenação de múltiplas colunas na tabela
    - Maximizar e redimensionar tela
    - Totais de linhas e colunas

--- v1.0/UMain.dfm
+++ v1.0/UMain.dfm
@@ -1,9 +1,9 @@
 object fMain: TfMain
-  Left = 266
-  Top = 124
-  Width = 1305
-  Height = 675
-  Caption = 'fMain'
+  Left = 426
+  Top = 128
+  Width = 1168
+  Height = 650
+  Caption = 'Visualizar DataSet'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,32 +11,297 @@ object fMain: TfMain
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  ShowHint = True
+  OnClose = FormClose
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
-  object gdMain: TDBGrid
+  object pnDados: TPanel
+    Left = 287
+    Top = 0
+    Width = 865
+    Height = 587
+    Align = alClient
+    BevelOuter = bvNone
+    TabOrder = 0
+    object gdMain: TDBGrid
+      Left = 0
+      Top = 0
+      Width = 865
+      Height = 587
+      Align = alClient
+      DataSource = dsMain
+      TabOrder = 0
+      TitleFont.Charset = DEFAULT_CHARSET
+      TitleFont.Color = clWindowText
+      TitleFont.Height = -11
+      TitleFont.Name = 'MS Sans Serif'
+      TitleFont.Style = []
+      OnTitleClick = gdMainTitleClick
+    end
+    object mmOrder: TMemo
+      Left = 102
+      Top = 316
+      Width = 335
+      Height = 205
+      Enabled = False
+      TabOrder = 1
+      Visible = False
+    end
+    object mmDescend: TMemo
+      Left = 456
+      Top = 316
+      Width = 335
+      Height = 205
+      Enabled = False
+      TabOrder = 2
+      Visible = False
+    end
+  end
+  object pnLado: TPanel
     Left = 0
     Top = 0
-    Width = 1289
-    Height = 636
-    Align = alClient
-    DataSource = dsMain
-    TabOrder = 0
-    TitleFont.Charset = DEFAULT_CHARSET
-    TitleFont.Color = clWindowText
-    TitleFont.Height = -11
-    TitleFont.Name = 'MS Sans Serif'
-    TitleFont.Style = []
+    Width = 287
+    Height = 587
+    Align = alLeft
+    BevelOuter = bvNone
+    TabOrder = 1
+    OnResize = pnLadoResize
+    object pnLadoSuperior: TPanel
+      Left = 1
+      Top = 1
+      Width = 285
+      Height = 270
+      BevelOuter = bvNone
+      ParentColor = True
+      TabOrder = 0
+      object SpeedButton1: TSpeedButton
+        Left = 254
+        Top = 13
+        Width = 30
+        Height = 26
+        Hint = 'Abrir arquivo'
+        Flat = True
+        Glyph.Data = {
+          F6060000424DF606000000000000360000002800000018000000180000000100
+          180000000000C006000000000000000000000000000000000000000000000000
+          0000000000000000000000000000000000000000000000000000000000000000
+          0000000000000000000000000000000000000000000000000000000000000000
+          00009B670A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A
+          9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A0A9F6A
+          0A9F6A0A9F6A0A98650AD0A634FFFFFFF6F4EFF8F8F8F7F7F7F7F7F7F7F7F7C2
+          C2C0FBFBFBF5F5F5F5F5F5F4F4F4F3F3F3F1F1F1F0F0F0BFBEBDF8F8F8EDEDED
+          ECECEBEBEBEAEAEAE9E8E8E8FFFFFF9F6A0AD0A634FFFFFFF8F8F8F8F8F8F7F7
+          F7F7F7F7F7F7F7C2C1C0FBFBFBF6F6F6F4F4F4F4F4F3F3F3F3F2F2F2F0F0F0BF
+          BEBDF8F8F8EDEDECECECEBEAEAEAE9E9E9E8E8E7FFFFFF9F6A0AD0A634FFFFFF
+          C3C3C2C3C2C1C3C2C1C2C2C0C2C2C0ADACAAE7E6E6C2C1C0C2C1C0C1C0BFC1C0
+          BFC1C0BFC0C0BEACABAAE5E5E5BFBEBDBEBEBCBEBDBCBDBDBBBDBCBBFFFFFF9F
+          6A0AD0A634FFFFFFE8E8E8E6E6E6E6E6E6E6E6E6E6E6E5BBBBB9F5F5F5E5E5E5
+          E5E5E5E5E5E5E4E4E3E4E4E3E3E3E3BAB9B8F3F3F3E0E0E0E0E0DFDFDFDFDEDE
+          DEDEDEDDFFFFFF9F6A0AD0A634FFFFFFF5F5F5F5F5F5F5F5F5F5F5F5F5F5F5C1
+          C1C0FBFBFBF5F5F5F4F4F4F4F4F4F4F4F4F4F4F4F4F4F4C1C0BFFAFAFAF3F3F3
+          F3F3F2F2F2F2F2F2F2F2F2F1FFFFFF9F6A0AD0A634FFFFFFBDBCBBBBBBBABBBB
+          BABBBBB9BBBBB9AAAAA8E4E3E3BBBAB9BBBAB9BBBAB9BBBAB9BBBAB9BBBAB8AA
+          A9A7E3E3E3BAB9B8B9B8B7B9B8B7B9B8B6B9B8B7FFFFFF9F6A0AD0A634FFFFFF
+          FAFAFAFAFAFAF9F9F9F9F9F9F9F9F9C3C2C1FCFCFCF7F7F7F7F7F7F7F7F7F6F6
+          F6F6F6F6F6F6F6C1C1C0FBFBFBF3F3F3F2F2F2F0F0F0EFEFEFEEEEEEFFFFFF9F
+          6A0AD0A634FFFFFFFBFBFBFAFAFAFAFAFAF9F9F9F9F9F9C3C2C1FCFCFCF8F8F8
+          F7F7F7F7F7F7F7F7F7F6F6F6F6F6F6C2C1C0FBFBFBF4F4F4F3F3F3F2F2F2F1F1
+          F1EFEFEFFFFFFF9F6A0AD0A634FFFFFFFBFBFBFBFBFBFAFAFAFAFAFAF9F9F9C3
+          C2C1FDFDFDF8F8F8F8F8F8F7F7F7F7F7F7F7F7F7F6F6F6C2C1C0FBFBFBF5F5F5
+          F4F4F4F3F3F3F2F2F2F1F1F1FFFFFF9F6A0AD0A634FFFFFFC5C4C3C4C3C2C4C3
+          C2C3C3C2C3C3C2ADACABE7E7E6C3C2C1C3C2C1C3C2C1C2C2C0C2C2C0C2C2C0AD
+          ACAAE7E6E6C2C1C0C1C1C0C1C0BFC1C0BFC0C0BEFFFFFF9F6A0AD0A634FFFFFF
+          E9E9E9E7E7E7E7E7E7E7E7E7E7E7E6BCBBBAF5F5F5E6E6E6E6E6E6E6E6E6E7E7
+          E6E7E7E6E6E6E6BBBBBAF5F5F5E5E5E5E4E4E4E4E4E3E3E3E2E3E3E3FFFFFF9F
+          6A0AD0A634FFFFFFF6F6F6F6F6F6F5F5F5F5F5F5F5F5F5C1C1C0FBFBFBF5F5F5
+          F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5C1C1C0FBFBFBF5F5F5F5F5F5F5F5F5F5F5
+          F5F4F4F4FFFFFFA37116D0A634FFFFFFBEBDBCBDBCBABDBCBBBDBCBBBDBCBBAB
+          AAA8E5E4E4BDBCBBBCBCBABCBCBABCBCBABCBCBABCBCBAABAAA8E4E4E3BCBBBA
+          BCBBBABBBBBABBBBBABCBBBAFFFFFFAF8332D2A838FFFFFFFDFDFDFDFDFDFCFC
+          FCFCFCFCFCFCFCC4C4C2FEFEFEFCFCFCFCFCFCFCFCFCFBFBFBFBFBFBFAFAFAC3
+          C3C2FDFDFDF9F9F9F9F9F9F9F9F9F8F8F8F8F8F8FFFFFFB28739DCBB64FFFFFF
+          FEFEFEFDFDFDFDFDFDFCFCFCFCFCFCC4C4C2FEFEFEFCFCFCFCFCFCFCFCFCFCFC
+          FCFBFBFBFBFBFBC3C3C2FDFDFDFAFAFAF9F9F9F9F9F9F9F9F9F8F8F8FFFFFFB3
+          893DE2C883FFFFFFFDFDFDFDFDFDFDFDFDFCFCFCFCFCFCC4C4C2FEFEFEFCFCFC
+          FCFCFCFCFCFCFCFCFCFCFCFCFBFBFBC4C3C2FDFDFDFAFAFAFAFAFAFAFAFAFAFA
+          FAF9F9F9FFFFFFB58C42E4CB89EED3ABE7C187E7C28AE8C289E5BC7FE1B26AE0
+          AC5EDFAA5ADEA959DEA959DEA957DEA957DDA756DDA754DDA754DDA652DDA551
+          DDA551DDA450DDA450DCA44EE6BE82B68E46E5CF90F4E2C2EFD4A1F0D5A4EED0
+          9AEBC887E7C077E6BA6AE5B866E5B866E5B864E5B864E4B863E4B863E3B661E3
+          B660E3B660E3B55EE3B55EE3B45DE3B35CE3B35CEBCD93B9914BE8D39AF7EAD2
+          F2DDB7F1DAAFEED29DEBCA8AE8C27BE6BC6EE5BB6AE5BA69E5BA69E5B967E5B8
+          66E5B866E5B864E5B864E4B863E4B863E3B661E3B661E3B660E3B55EECD19DBA
+          944FEAD7A3F9F0DFF4E3C2F1DBB3EED39EECCB8EE9C47EE6BE71E6BB6DE5BB6B
+          E5BB6BE5BB6AE5BB6AE5BA69E5BA69E5B967E5B866E5B866E5B864E5B864E4B8
+          63E4B863EED6A7BC9754EBD9AAFBF4E8F9F1E1F8EDD9F5E8CFF4E5C7F2E0BFF1
+          DEB8F1DCB6F1DCB5F1DCB5F1DCB5F1DCB5F0DBB4F0DBB4F0DAB4F0DAB4F0DAB2
+          F0DAB2EFDAB2EFDAB2EFDAB1EFDAB1BE9957EDDCAFEBDAABEAD8A5E9D59EE7D2
+          97E5CE8EE3CB88E3C982E2C77FE2C77FE2C67EE2C67EE2C67EE1C67DE1C67DE0
+          C57CE0C57CE0C57BE0C57BE0C579E0C579E0C378E0C378DFC377}
+        OnClick = SpeedButton1Click
+      end
+      object lbDiretorio: TLabel
+        Left = 8
+        Top = 2
+        Width = 42
+        Height = 13
+        Caption = 'Diretório:'
+      end
+      object lbFiltroArquivos: TLabel
+        Left = 8
+        Top = 40
+        Width = 83
+        Height = 13
+        Caption = 'Filtro de arquivos:'
+      end
+      object edDiretorio: TEdit
+        Left = 5
+        Top = 16
+        Width = 247
+        Height = 21
+        TabOrder = 0
+        OnKeyUp = edDiretorioKeyUp
+      end
+      object clbArquivos: TCheckListBox
+        Left = 5
+        Top = 78
+        Width = 275
+        Height = 185
+        Anchors = [akLeft, akTop, akBottom]
+        ItemHeight = 13
+        TabOrder = 1
+        OnDblClick = clbArquivosDblClick
+      end
+      object edFiltroArq: TEdit
+        Left = 5
+        Top = 54
+        Width = 275
+        Height = 21
+        TabOrder = 2
+        OnKeyUp = edFiltroArqKeyUp
+      end
+    end
+    object pnLadoInferior: TPanel
+      Left = 1
+      Top = 272
+      Width = 285
+      Height = 315
+      BevelOuter = bvNone
+      ParentColor = True
+      TabOrder = 1
+      object lbFiltroClientDataSet: TLabel
+        Left = 8
+        Top = 2
+        Width = 103
+        Height = 13
+        Caption = 'Filter do clientdataset:'
+      end
+      object bAplicarFiltro: TSpeedButton
+        Left = 258
+        Top = 30
+        Width = 23
+        Height = 51
+        Hint = 'Aplicar filter no ClientDataSet'
+        Caption = '>'
+        OnClick = bAplicarFiltroClick
+      end
+      object lbFiltroColunas: TLabel
+        Left = 8
+        Top = 102
+        Width = 80
+        Height = 13
+        Caption = 'Filtro de colunas:'
+      end
+      object mmFilter: TMemo
+        Left = 5
+        Top = 16
+        Width = 250
+        Height = 79
+        TabOrder = 0
+      end
+      object clbColunas: TCheckListBox
+        Left = 5
+        Top = 140
+        Width = 275
+        Height = 175
+        OnClickCheck = clbColunasClickCheck
+        Anchors = [akLeft, akTop, akBottom]
+        ItemHeight = 13
+        TabOrder = 1
+      end
+      object edFiltroColunas: TEdit
+        Left = 5
+        Top = 116
+        Width = 216
+        Height = 21
+        TabOrder = 2
+        OnKeyUp = edFiltroColunasKeyUp
+      end
+      object chkTudo: TCheckBox
+        Left = 226
+        Top = 118
+        Width = 54
+        Height = 17
+        Hint = 'Marcar / desmarcar todas abaixo'
+        Caption = 'Todas'
+        TabOrder = 3
+        OnClick = chkTudoClick
+      end
+    end
+  end
+  object pnRodape: TPanel
+    Left = 0
+    Top = 587
+    Width = 1152
+    Height = 24
+    Align = alBottom
+    BevelOuter = bvNone
+    TabOrder = 2
+    object sbInfo: TStatusBar
+      Left = 0
+      Top = -1
+      Width = 1152
+      Height = 25
+      Panels = <
+        item
+          Width = 100
+        end
+        item
+          Width = 150
+        end
+        item
+          Width = 100
+        end
+        item
+          Width = 150
+        end
+        item
+          Width = 0
+        end>
+      SimplePanel = False
+    end
   end
   object cdsMain: TClientDataSet
     Aggregates = <>
+    FieldDefs = <>
+    IndexDefs = <
+      item
+        Name = 'cdsMainIndex1'
+      end>
     Params = <>
-    Left = 64
-    Top = 64
+    StoreDefs = True
+    Left = 382
+    Top = 56
   end
   object dsMain: TDataSource
     DataSet = cdsMain
-    Left = 98
-    Top = 64
+    Left = 416
+    Top = 56
+  end
+  object dlgAbrirArquivo: TOpenDialog
+    InitialDir = '%systemdrive%'
+    Left = 190
+    Top = 6
   end
 end
--- v1.0/UMain.pas
+++ v1.0/UMain.pas
@@ -4,20 +4,75 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs, DB,
-  DBClient, Grids, DBGrids;
+  DBClient, Grids, DBGrids, StdCtrls, CheckLst, Buttons, ExtCtrls, ComCtrls;
 
 type
+  TInformacoesRodape = (irLinhas, irLinhasFiltradas, irColunas, irColunasVisiveis, irArquivo);
+
   TfMain = class(TForm)
     cdsMain: TClientDataSet;
-    gdMain: TDBGrid;
     dsMain: TDataSource;
+    pnDados: TPanel;
+    gdMain: TDBGrid;
+    pnLado: TPanel;
+    dlgAbrirArquivo: TOpenDialog;
+    pnLadoSuperior: TPanel;
+    pnLadoInferior: TPanel;
+    SpeedButton1: TSpeedButton;
+    edDiretorio: TEdit;
+    clbArquivos: TCheckListBox;
+    edFiltroArq: TEdit;
+    lbFiltroClientDataSet: TLabel;
+    bAplicarFiltro: TSpeedButton;
+    mmFilter: TMemo;
+    clbColunas: TCheckListBox;
+    edFiltroColunas: TEdit;
+    chkTudo: TCheckBox;
+    mmOrder: TMemo;
+    mmDescend: TMemo;
+    pnRodape: TPanel;
+    sbInfo: TStatusBar;
+    lbDiretorio: TLabel;
+    lbFiltroArquivos: TLabel;
+    lbFiltroColunas: TLabel;
     procedure FormCreate(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure gdMainTitleClick(Column: TColumn);
+    procedure clbArquivosDblClick(Sender: TObject);
+    procedure edFiltroArqKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+    procedure edDiretorioKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+    procedure bAplicarFiltroClick(Sender: TObject);
+    procedure ConfigurarFiltroDataSet(const sFiltro: string);
+    procedure edFiltroColunasKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+    procedure clbColunasClickCheck(Sender: TObject);
+    procedure chkTudoClick(Sender: TObject);
+    procedure pnLadoResize(Sender: TObject);
   private
     { Private declarations }
     FsParametroInicializacao: string;
+    FsExtensoes: string;
+    FoItemsChecked: TStringList;
+    FoItemsUnchecked: TStringList;
 
     procedure ArmazenarParametroInicializacao;
-    procedure CarregarArquivoDados(const sArquivo: string);
+    procedure CarregarArquivoDados(const psArquivo: string);
+    procedure ListarOutrosArquivos(const psDiretorio, psFiltro: string);
+    function TestarFiltroNomeArquivo(const psNomeArquivo: string): boolean;
+
+    function RetornarNomeArquivoINI: string;
+    procedure CarregarConfiguracao;
+    procedure SalvarConfiguracao;
+
+    procedure CarregarColunas;
+    function RetornarListaDeColunas: string;
+    procedure RemarcarItens;
+
+    procedure AdicionarNaLista(oLista: TStringList; const sColuna: string);
+    procedure RemoverDaLista(oLista: TStringList; const sColuna: string);
+    procedure ControleDasListas(const sColuna: string; const bChecked: boolean);
+
+    procedure MostrarMensagemRodape(irInformacao: TInformacoesRodape; const sTexto: string);
   public
     { Public declarations }
   end;
@@ -25,27 +80,390 @@ type
 var
   fMain: TfMain;
 
+const
+  S_EXTENSOES_PADRAO: string =
+    '*.chd; *.dat|*.chd; *.dat|*.chd|*.chd|*.dat|*.dat|Todos os arquivos (*.*)|*.*';
+
 implementation
 
 {$R *.DFM}
 
+uses
+  IniFiles, StrUtils;
+
 procedure TfMain.FormCreate(Sender: TObject);
 begin
+  FoItemsChecked := TStringList.Create; //PC_OK
+  FoItemsChecked.Clear;
+  FoItemsUnchecked := TStringList.Create; //PC_OK
+  FoItemsUnchecked.Clear;
+
+  CarregarConfiguracao;
   ArmazenarParametroInicializacao;
   CarregarArquivoDados(FsParametroInicializacao);
+  ListarOutrosArquivos(edDiretorio.Text, '');
+end;
+
+procedure TfMain.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  FreeAndNil(FoItemsChecked); //PC_OK
+  FreeAndNil(FoItemsUnchecked); //PC_OK
+
+  SalvarConfiguracao;
 end;
 
 procedure TfMain.ArmazenarParametroInicializacao;
 begin
   FsParametroInicializacao := Trim(ParamStr(1));
+
+  if FsParametroInicializacao <> '' then
+    edDiretorio.Text := ExtractFilePath(FsParametroInicializacao);
 end;
 
-procedure TfMain.CarregarArquivoDados(const sArquivo: string);
+procedure TfMain.CarregarArquivoDados(const psArquivo: string);
 begin
-  if (sArquivo = '') or (not FileExists(sArquivo)) then
+  if (psArquivo = '') or (not FileExists(psArquivo)) then
     Exit;
 
-  cdsMain.LoadFromFile(sArquivo);
+  ConfigurarFiltroDataSet('');
+  cdsMain.LoadFromFile(psArquivo);
+
+  MostrarMensagemRodape(irLinhas, IntToStr(cdsMain.RecordCount));
+  MostrarMensagemRodape(irLinhasFiltradas, IntToStr(cdsMain.RecordCount));
+  MostrarMensagemRodape(irColunas, IntToStr(cdsMain.Fields.Count));
+  MostrarMensagemRodape(irColunasVisiveis, IntToStr(cdsMain.Fields.Count));
+  MostrarMensagemRodape(irArquivo, psArquivo);
+
+  CarregarColunas;
+end;
+
+procedure TfMain.SpeedButton1Click(Sender: TObject);
+begin
+  if edDiretorio.Text <> '' then
+    dlgAbrirArquivo.InitialDir := edDiretorio.Text;
+  dlgAbrirArquivo.Filter := FsExtensoes;
+
+  if not dlgAbrirArquivo.Execute then
+    Exit;
+
+  edDiretorio.Text := ExtractFilePath(dlgAbrirArquivo.FileName);
+  CarregarArquivoDados(dlgAbrirArquivo.FileName);
+  ListarOutrosArquivos(edDiretorio.Text, '');
+end;
+
+function TfMain.RetornarNomeArquivoINI: string;
+begin
+  result := ChangeFileExt(Application.ExeName, '.ini');
+end;
+
+procedure TfMain.CarregarConfiguracao;
+var
+  fArquivo: TIniFile;
+begin
+  fArquivo := TIniFile.Create(RetornarNomeArquivoINI);
+  try
+    FsExtensoes := fArquivo.ReadString('Config', 'Extensoes', S_EXTENSOES_PADRAO);
+    if FsExtensoes = '' then
+      FsExtensoes := S_EXTENSOES_PADRAO;
+    mmFilter.Lines.Text := fArquivo.ReadString('Config', 'Filtro', '');
+    edDiretorio.Text := fArquivo.ReadString('Config', 'Diretorio', 'C:\');
+  finally
+    fArquivo.Free;
+  end;
+end;
+
+procedure TfMain.SalvarConfiguracao;
+var
+  fArquivo: TIniFile;
+begin
+  fArquivo := TIniFile.Create(RetornarNomeArquivoINI);
+  try
+    fArquivo.WriteString('Config', 'Extensoes', FsExtensoes);
+    fArquivo.WriteString('Config', 'Filtro', mmFilter.Lines.Text);
+    fArquivo.WriteString('Config', 'Diretorio', edDiretorio.Text);
+  finally
+    fArquivo.Free;
+  end;
+end;
+
+procedure TfMain.ListarOutrosArquivos(const psDiretorio, psFiltro: string);
+var
+  srPesquisa: TSearchRec;
+  sPath: string;
+begin
+  clbArquivos.Items.Clear;
+
+  try
+    sPath := IncludeTrailingBackslash(psDiretorio) + '*' + psFiltro + '*.*';
+    if FindFirst(sPath, faAnyFile - faDirectory, srPesquisa) <> 0 then
+      Exit;
+
+    repeat
+      if TestarFiltroNomeArquivo(srPesquisa.Name) then
+        clbArquivos.Items.Add(srPesquisa.Name);
+    until FindNext(srPesquisa) <> 0;
+  finally
+    FindClose(srPesquisa);
+  end;
+end;
+
+function TfMain.TestarFiltroNomeArquivo(const psNomeArquivo: string): boolean;
+begin
+  result := (Pos(UpperCase(ExtractFileExt(psNomeArquivo)), UpperCase(FsExtensoes)) > 0) and
+    ((Pos(UpperCase(edFiltroArq.Text), UpperCase(psNomeArquivo)) > 0) or
+    (edFiltroArq.Text = EmptyStr));
+end;
+
+procedure TfMain.gdMainTitleClick(Column: TColumn);
+const
+  sIdxMain: string = 'idxMain';
+var
+  bControl: boolean;
+  sCampoClicado: string;
+  iPosOrder, iPosDescend: integer;
+  slListaIndices: TStrings;
+  iPosIndice: integer;
+  oIndexDef: TIndexDef;
+begin
+  inherited;
+
+  bControl := (GetKeyState(VK_CONTROL) < 0);
+  sCampoClicado := Column.FieldName;
+
+  iPosOrder := mmOrder.Lines.IndexOf(sCampoClicado);
+  iPosDescend := mmDescend.Lines.IndexOf(sCampoClicado);
+  if (not bControl) then
+  begin
+    if mmOrder.Lines.Count > 1 then
+      iPosOrder := -1;
+    mmOrder.Lines.Text := sCampoClicado;
+    if (iPosOrder < 0) then
+      mmDescend.Lines.Clear
+    else
+    begin
+      if (iPosDescend < 0) then
+        mmDescend.Lines.Add(sCampoClicado)
+      else
+        mmDescend.Lines.Delete(iPosDescend);
+    end;
+  end
+  else
+  begin
+    if iPosOrder < 0 then
+    begin
+      mmOrder.Lines.Add(sCampoClicado);
+      if iPosDescend >= 0 then
+        mmDescend.Lines.Delete(iPosDescend);
+    end
+    else
+    begin
+      if (iPosDescend >= 0) then
+        mmDescend.Lines.Delete(iPosDescend)
+      else if (iPosDescend < 0) then
+        mmDescend.Lines.Add(sCampoClicado);
+    end;
+  end;
+
+  slListaIndices := TStringList.Create;
+  try
+    slListaIndices.Clear;
+    TClientDataSet(Column.Grid.DataSource.DataSet).GetIndexNames(slListaIndices);
+    iPosIndice := slListaIndices.IndexOf(sIdxMain);
+    if iPosIndice >= 0 then
+      TClientDataSet(Column.Grid.DataSource.DataSet).DeleteIndex(sIdxMain);
+
+    TClientDataSet(Column.Grid.DataSource.DataSet).IndexDefs.Update;
+
+    oIndexDef := TClientDataSet(Column.Grid.DataSource.DataSet).IndexDefs.AddIndexDef;
+    oIndexDef.Name := sIdxMain;
+    oIndexDef.Fields := StringReplace(mmOrder.Lines.CommaText, ',', ';', [rfReplaceAll]);
+    oIndexDef.Options := [];
+    oIndexDef.DescFields := StringReplace(mmDescend.Lines.CommaText, ',', ';', [rfReplaceAll]);
+    TClientDataSet(Column.Grid.DataSource.DataSet).IndexName := sIdxMain;
+  finally
+    FreeAndNil(slListaIndices);
+  end;
+end;
+
+procedure TfMain.clbArquivosDblClick(Sender: TObject);
+begin
+  if clbArquivos.ItemIndex < 0 then
+    Exit;
+
+  CarregarArquivoDados(IncludeTrailingBackslash(edDiretorio.Text) +
+    clbArquivos.items[clbArquivos.ItemIndex]);
+end;
+
+procedure TfMain.edFiltroArqKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+begin
+  ListarOutrosArquivos(edDiretorio.Text, edFiltroArq.Text);
+end;
+
+procedure TfMain.edDiretorioKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+begin
+  ListarOutrosArquivos(edDiretorio.Text, edFiltroArq.Text);
+end;
+
+procedure TfMain.bAplicarFiltroClick(Sender: TObject);
+begin
+  ConfigurarFiltroDataSet(mmFilter.Lines.Text);
+end;
+
+procedure TfMain.ConfigurarFiltroDataSet(const sFiltro: string);
+begin
+  try
+    cdsMain.DisableControls;
+
+    cdsMain.Filtered := False;
+    if sFiltro <> '' then
+    begin
+      cdsMain.Filter := sFiltro;
+      cdsMain.Filtered := True;
+
+      MostrarMensagemRodape(irLinhasFiltradas, IntToStr(cdsMain.RecordCount));
+    end;
+  finally
+    cdsMain.EnableControls;
+  end;
+end;
+
+procedure TfMain.CarregarColunas;
+var
+  i: integer;
+begin
+  clbColunas.Items.Clear;
+
+  clbColunas.Items.Text := RetornarListaDeColunas;
+  for i := 0 to clbColunas.Items.Count - 1 do
+    clbColunas.Checked[i] := True;
+  FoItemsChecked.Text := clbColunas.Items.Text;
+  FoItemsUnchecked.Clear;
+  chkTudo.Checked := True;
+end;
+
+procedure TfMain.edFiltroColunasKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+var
+  i: integer;
+  cols: TStringList;
+  sFiltro: string;
+begin
+  cols := TStringList.Create;
+  try
+    cols.Text := RetornarListaDeColunas;
+    for i := cols.Count - 1 downto 0 do
+    begin
+      sFiltro := AnsiUpperCase(edFiltroColunas.Text);
+      if (Pos(sFiltro, AnsiUpperCase(cols[i])) = 0) and (sFiltro <> '') then
+        cols.Delete(i);
+    end;
+  finally
+    clbColunas.Items.Text := cols.Text;
+    FreeAndNil(cols);
+    RemarcarItens;
+  end;
+end;
+
+procedure TfMain.clbColunasClickCheck(Sender: TObject);
+begin
+  ControleDasListas(clbColunas.Items[clbColunas.ItemIndex],
+    clbColunas.Checked[clbColunas.ItemIndex]);
+end;
+
+function TfMain.RetornarListaDeColunas: string;
+var
+  i: integer;
+begin
+  result := '';
+
+  if not cdsMain.isEmpty then
+  begin
+    for i := 0 to cdsMain.FieldDefs.Count - 1 do
+      result := result + cdsMain.FieldDefs.Items[i].Name + #13#10;
+  end;
+end;
+
+procedure TfMain.RemarcarItens;
+var
+  i: integer;
+  sColuna: string;
+begin
+  for i := 0 to clbColunas.Items.Count - 1 do
+  begin
+    sColuna := clbColunas.Items[i];
+    if FoItemsUnchecked.IndexOf(sColuna) >= 0 then
+      clbColunas.Checked[i] := False;
+    if FoItemsChecked.IndexOf(sColuna) >= 0 then
+      clbColunas.Checked[i] := True;
+  end;
+end;
+
+procedure TfMain.chkTudoClick(Sender: TObject);
+var
+  i: integer;
+begin
+  try
+    cdsMain.DisableControls;
+
+    for i := 0 to clbColunas.Items.Count - 1 do
+    begin
+      clbColunas.Checked[i] := chkTudo.Checked;
+      ControleDasListas(clbColunas.Items[i], chkTudo.Checked);
+    end;
+  finally
+    cdsMain.EnableControls;
+  end;
+end;
+
+procedure TfMain.AdicionarNaLista(oLista: TStringList; const sColuna: string);
+var
+  iPos: integer;
+begin
+  iPos := oLista.IndexOf(sColuna);
+  if iPos < 0 then
+    oLista.Add(sColuna);
+end;
+
+procedure TfMain.RemoverDaLista(oLista: TStringList; const sColuna: string);
+var
+  iPos: integer;
+begin
+  iPos := oLista.IndexOf(sColuna);
+  if iPos >= 0 then
+    oLista.Delete(iPos);
+end;
+
+procedure TfMain.ControleDasListas(const sColuna: string; const bChecked: boolean);
+begin
+  if bChecked then
+  begin
+    AdicionarNaLista(FoItemsChecked, sColuna);
+    RemoverDaLista(FoItemsUnchecked, sColuna);
+  end
+  else
+  begin
+    AdicionarNaLista(FoItemsUnchecked, sColuna);
+    RemoverDaLista(FoItemsChecked, sColuna);
+  end;
+  cdsMain.FieldByName(sColuna).Visible := bChecked;
+  MostrarMensagemRodape(irColunasVisiveis, IntToStr(FoItemsChecked.Count));
+end;
+
+procedure TfMain.pnLadoResize(Sender: TObject);
+begin
+  pnLadoSuperior.Height := Trunc(pnLado.Height / 2);
+  pnLadoInferior.Top := pnLadoSuperior.Height;
+  pnLadoInferior.Height := Trunc(pnLado.Height / 2);
+end;
+
+procedure TfMain.MostrarMensagemRodape(irInformacao: TInformacoesRodape; const sTexto: string);
+begin
+  case irInformacao of
+    irLinhas: sbInfo.Panels[Ord(irInformacao)].Text := 'Linhas: ' + sTexto;
+    irLinhasFiltradas: sbInfo.Panels[Ord(irInformacao)].Text := 'Linhas filtradas: ' + sTexto;
+    irColunas: sbInfo.Panels[Ord(irInformacao)].Text := 'Colunas: ' + sTexto;
+    irColunasVisiveis: sbInfo.Panels[Ord(irInformacao)].Text := 'Colunas visíveis: ' + sTexto;
+    irArquivo: sbInfo.Panels[Ord(irInformacao)].Text := 'Arquivo: ' + sTexto;
+  end;
 end;
 
 end.
